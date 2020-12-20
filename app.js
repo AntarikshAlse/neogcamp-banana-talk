0af9b46967c885acf8fb960c3cf6913f65ba6375
Antariksh Alse <74238119+Int-coder@users.noreply.github.com>
2020-12-20 13:32:57 +0530

    added catch block

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ var output = document.querySelector("#bn-output");
  //   var serverURL = "https://reqres.in/api/users"  + "/" + input.value;
  //    json.data.first_name +" "+ json.data.last_name;
 
+function errorHandler(error) {
+     console.log("error occured", error);
+     alert("something wrong with server! try again after some time")
+    }
+
 function translate (){
       var serverURL = "https://api.funtranslations.com/translate/minion.json" +  "?" + "text=" + input.value ;
     fetch(serverURL)  
@@ -13,36 +18,8 @@ function translate (){
     .then(json => {
         var translatedText = json.contents.translated;
          output.innerText = translatedText;})
+ .catch(errorHandler)
 }
 
 btntranslate.addEventListener("click",translate)
 
-
-/* var btnTranslate = document.querySelector("#btn-translate");
-var txtInput = document.querySelector("#txt-input");
-var outputDiv = document.querySelector("#output");
-
- //var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"+"?" + "text=" + txtInput.innerHTML;
-
- 
- 
- function errorHandler(error) {
-     console.log("error occured", error);
-     alert("something wrong with server! try again after some time")
-    }
-    
-    
-    function clickHandler() {
-    var serverURL = "https://api.funtranslations.com/translate/minion.json"+"?" + "text=" + txtInput.value;
-                                                                        //var serverURL = "https://reqres.in/api/users"  + "/" + txtInput.value; 
-    // calling server for processing
-        fetch(serverURL)
-        .then(response => response.json())                                 
-        .then(json => {
-            var translatedText =  json.contents.translated;             //json.data.first_name +" "+ json.data.last_name;
-            outputDiv.innerText = translatedText; // output
-           })
-        .catch(errorHandler)
-};
-
-btnTranslate.addEventListener("click", clickHandler) */
